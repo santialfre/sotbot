2c2777df455790b09e110cffe9623ee05d508775
santialfre <santialfre@hotmail.com>
2020-06-21 01:52:41 -0500

    Add edit inlinekeyboard and should quest

--- sotbot.py
+++ sotbot.py
@@ -27,17 +27,9 @@ def fish(update, context):
 
 def fishtime(update, context):
     imgtimefish='https://i.redd.it/stvrzc6c2xi41.jpg'
-    strfishtime="""Tiempo para cocinar los peces:
-    [Fish](https://reloj-alarma.es/temporizador/#countdown=00:00:45&enabled=0&seconds=45&title=The+fish+is+already+done!) = 45 seconds
-    [Meat](https://reloj-alarma.es/temporizador/#countdown=00:01:05&enabled=0&seconds=65&title=The+fish+is+already+done!) (chicken, snake, pork, shark) = 65 seconds
-    [Trophy Fish](https://reloj-alarma.es/temporizador/#countdown=00:01:35&enabled=0&seconds=95&title=The+fish+is+already+done!) = 95 seconds
-    [Kraken](https://reloj-alarma.es/temporizador/#countdown=00:02:05&enabled=0&seconds=125&title=The+fish+is+already+done!) = 125 seconds
-    [Megalodon](https://reloj-alarma.es/temporizador/#countdown=00:02:05&enabled=0&seconds=125&title=The+fish+is+already+done!) = 125 seconds
-    
-
-    Crate a timer presing in each item"""
+    strfishtime=text.fishtime+'\n\n Creates a timer presing in each item'
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=imgtimefish, parse_mode= 'Markdown', caption=strfishtime)
-    print('foto enviada a: '+update.message.from_user.first_name)
+    print('foto enviada a: '+update.message.from_user.username)
 
 def talltales(update, context):
     buttonstalltales=[[Inlinebutton(text='The Shroudbreaker (Quest)', callback_data='theshroudbreakerquest')],
@@ -48,24 +40,28 @@ def talltales(update, context):
     'The Shroudbreaker Arc: \n',reply_markup= Inlinekeyboard_talltales)
     print(update.inline_query)
 
-def urltest(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, parse_mode='Markdown', text="Para continuar toque [aquí](http://www.google.com/)")
-    print("link enviado a" + update.effective_chat.first_name + update.effective_chat.last_name)
+#def urltest(update, context):
+#    context.bot.send_message(chat_id=update.effective_chat.id, parse_mode='Markdown', text="Para continuar toque [aquí](http://www.google.com/)")
+#    print("link enviado a" + update.effective_chat.first_name + update.effective_chat.last_name)
 
 def inlineresult(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text.theshroudbreakerquest_text)
-    print(update.callback_query.from_user)
-    context.bot.answer_callback_query(update.callback_query.id)
+    context.bot.edit_message_reply_markup(chat_id=update.callback_query.message.chat.id, message_id=update.callback_query.message.message_id, reply_markup=telegram.InlineKeyboardMarkup([[]]))
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=text.theshroudbreakerquest_img, parse_mode='Markdown', 
+    caption=text.theshroudbreakerquest_text+
+    '\n\n Go to the [guide](https://rarethief.com/sea-of-thieves-the-shroudbreaker-tall-tale-guide/) if you need it.' )
+    print('info for: '+update.callback_query.from_user.username)
+    #print('Id: '+str(update.callback_query.message.message_id)) #In case of edit a message, add a photo to an existing message seams to be not possible
+    context.bot.answer_callback_query(update.callback_query.id) #In any inline keyboard for remove the clock
 
 inlineresult_handler=CallbackQueryHandler(inlineresult, pattern='theshroudbreakerquest')
-urltest_handler=CommandHandler('urltest', urltest)
+#urltest_handler=CommandHandler('urltest', urltest)
 talltales_handler=CommandHandler('talltales', talltales)
 start_handler=CommandHandler('start', start)
 fish_hundler=CommandHandler('fish', fish)
 fishtime_hundler=CommandHandler('fishtime', fishtime)
 
 dispatcher.add_handler(inlineresult_handler)
-dispatcher.add_handler(urltest_handler)
+#dispatcher.add_handler(urltest_handler)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(fish_hundler)
 dispatcher.add_handler(fishtime_hundler)
--- text.py
+++ text.py
@@ -1,1 +1,8 @@
 theshroudbreakerquest_text="""The Shroudbreaker is the first Tall Tale in the Shores of Gold Arc. The Tale Book can be voted for on the table next to the Mysterious Stranger in the Tavern of any Outpost. When players progress through Tall Tales, they will unlock Checkpoints. If they wish to continue the Tall Tale at a later time, they can vote on any acquired Checkpoints at the Voyage Table."""
+theshroudbreakerquest_img='https://gamepedia.cursecdn.com/seaofthieves_gamepedia/e/ed/The_Shroudbreaker_quest.png?version=75fee3537e174e902a880fd1c9fb48c6'
+fishtime="""Tiempo para cocinar los peces:
+    [Fish](https://reloj-alarma.es/temporizador/#countdown=00:00:45&enabled=0&seconds=45&title=The+fish+is+already+done!) = 45 seconds
+    [Meat](https://reloj-alarma.es/temporizador/#countdown=00:01:05&enabled=0&seconds=65&title=The+fish+is+already+done!) (chicken, snake, pork, shark) = 65 seconds
+    [Trophy Fish](https://reloj-alarma.es/temporizador/#countdown=00:01:35&enabled=0&seconds=95&title=The+fish+is+already+done!) = 95 seconds
+    [Kraken](https://reloj-alarma.es/temporizador/#countdown=00:02:05&enabled=0&seconds=125&title=The+fish+is+already+done!) = 125 seconds
+    [Megalodon](https://reloj-alarma.es/temporizador/#countdown=00:02:05&enabled=0&seconds=125&title=The+fish+is+already+done!) = 125 seconds"""
