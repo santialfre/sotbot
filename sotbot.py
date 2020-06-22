0ad2bfc78614ab129b9f449b029936ad3f809b5f
santialfre <santialfre@hotmail.com>
2020-06-21 19:57:57 -0500

    change for delete/edit message for tall tales

--- sotbot.py
+++ sotbot.py
@@ -45,10 +45,12 @@ def talltales(update, context):
 #    print("link enviado a" + update.effective_chat.first_name + update.effective_chat.last_name)
 
 def inlineresult(update, context):
-    context.bot.edit_message_reply_markup(chat_id=update.callback_query.message.chat.id, message_id=update.callback_query.message.message_id, reply_markup=telegram.InlineKeyboardMarkup([[]]))
+    #context.bot.edit_message_reply_markup(chat_id=update.callback_query.message.chat.id, message_id=update.callback_query.message.message_id, reply_markup=telegram.InlineKeyboardMarkup([[]]))
+    #Change for empty kayboard fo deleated message
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=text.theshroudbreakerquest_img, parse_mode='Markdown', 
     caption=text.theshroudbreakerquest_text+
     '\n\n Go to the [guide](https://rarethief.com/sea-of-thieves-the-shroudbreaker-tall-tale-guide/) if you need it.' )
+    context.bot.deleteMessage(chat_id=update.callback_query.message.chat_id, message_id=update.callback_query.message.message_id)
     print('info for: '+update.callback_query.from_user.username)
     #print('Id: '+str(update.callback_query.message.message_id)) #In case of edit a message, add a photo to an existing message seams to be not possible
     context.bot.answer_callback_query(update.callback_query.id) #In any inline keyboard for remove the clock
