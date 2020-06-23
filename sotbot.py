2a4dc499ac50c47db6c5b584c6b409c102a08e3d
santialfre <santialfre@hotmail.com>
2020-06-22 19:46:11 -0500

    Change Inline Key for tall to be anidated to all calls

--- sotbot.py
+++ sotbot.py
@@ -1,6 +1,6 @@
 import telegram
 import logging
-import text
+import text, re
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
 from telegram.ext import InlineQueryHandler
@@ -32,9 +32,9 @@ def fishtime(update, context):
     print('foto enviada a: '+update.message.from_user.username)
 
 def talltales(update, context):
-    buttonstalltales=[[Inlinebutton(text='The Shroudbreaker (Quest)', callback_data='theshroudbreakerquest')],
-    [Inlinebutton(text='The Cursed Rogue', callback_data='TheCursedRogue')], 
-    [Inlinebutton(text='The Legendary Storyteller', callback_data='TheLegendaryStoryteller')]]
+    buttonstalltales=[[Inlinebutton(text='The Shroudbreaker (Quest)', callback_data='tall_theshroudbreakerquest')],
+    [Inlinebutton(text='The Cursed Rogue', callback_data='tall_theCursedRogue')], 
+    [Inlinebutton(text='The Legendary Storyteller', callback_data='tall_theLegendaryStoryteller')]]
     Inlinekeyboard_talltales=telegram.InlineKeyboardMarkup(buttonstalltales)
     context.bot.send_message(chat_id=update.effective_chat.id, text= 'Here we have the Tall Tales of '+
     'The Shroudbreaker Arc: \n',reply_markup= Inlinekeyboard_talltales)
@@ -47,15 +47,19 @@ def talltales(update, context):
 def inlineresult(update, context):
     #context.bot.edit_message_reply_markup(chat_id=update.callback_query.message.chat.id, message_id=update.callback_query.message.message_id, reply_markup=telegram.InlineKeyboardMarkup([[]]))
     #Change for empty kayboard fo deleated message
-    context.bot.send_photo(chat_id=update.effective_chat.id, photo=text.theshroudbreakerquest_img, parse_mode='Markdown', 
-    caption=text.theshroudbreakerquest_text+
-    '\n\n Go to the [guide](https://rarethief.com/sea-of-thieves-the-shroudbreaker-tall-tale-guide/) if you need it.' )
-    context.bot.deleteMessage(chat_id=update.callback_query.message.chat_id, message_id=update.callback_query.message.message_id)
-    print('info for: '+update.callback_query.from_user.username)
-    #print('Id: '+str(update.callback_query.message.message_id)) #In case of edit a message, add a photo to an existing message seams to be not possible
-    context.bot.answer_callback_query(update.callback_query.id) #In any inline keyboard for remove the clock
+    if update.callback_query.data == 'tall_theshroudbreakerquest':
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=text.theshroudbreakerquest_img, parse_mode='Markdown', 
+        caption=text.theshroudbreakerquest_text+
+        '\n\n Go to the [guide](https://rarethief.com/sea-of-thieves-the-shroudbreaker-tall-tale-guide/) if you need it.' )
+        context.bot.deleteMessage(chat_id=update.callback_query.message.chat_id, message_id=update.callback_query.message.message_id)
+        print('info for: '+update.callback_query.from_user.username)
+        #print('Id: '+str(update.callback_query.message.message_id)) #In case of edit a message, add a photo to an existing message seams to be not possible
+        context.bot.answer_callback_query(update.callback_query.id) #In any inline keyboard for remove the clock
+    else:
+        context.bot.send_message(chat_id=update.callback_query.message.chat_id, text='There is no information at this momento for this tall tale.')
+        context.bot.answer_callback_query(update.callback_query.id)
 
-inlineresult_handler=CallbackQueryHandler(inlineresult, pattern='theshroudbreakerquest')
+inlineresult_handler=CallbackQueryHandler(inlineresult, pattern=r"tall_")
 #urltest_handler=CommandHandler('urltest', urltest)
 talltales_handler=CommandHandler('talltales', talltales)
 start_handler=CommandHandler('start', start)
